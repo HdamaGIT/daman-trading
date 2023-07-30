feac2cdc54169a47991a532f0fed22d7b03410db
hughdaman <HdamaGIT>
2023-07-30 21:54:21 +0100

    initial commit

--- trading_system/src/trading_strategies/strategy.py
+++ trading_system/src/trading_strategies/strategy.py
@@ -1,7 +1,3 @@
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 from datetime import time
 import pandas as pd
 
@@ -11,17 +7,13 @@ import pandas as pd
 #########################################################################################
 
 
-# This code defines three functions: signal, allocation, and plot_allocation. The signal function adds two columns to the input dataframe data: trigger and trigger_shift. The trigger column is created by using np.where to set the value to "BUY" if the value in the signal_EMA column is 1, "SELL" if the value in the signal_EMA column is 2, and "999" otherwise. The trigger_shift column is created by shifting the trigger column one time period back.
-# The allocation function adds three columns to the input dataframe data: trade, position, and position_shift. The trade column is created by using np.where to set the value to 1 if the current trigger value is "BUY" and the previous trigger value is not "BUY", to 2 if the current trigger value is "SELL" and the previous trigger value is not "SELL", and to 0 otherwise. The position column is created by using np.where to set the value to "long" if the trigger value is "BUY", "short" if the trigger value is "SELL", and np.nan otherwise. The position_shift column is created by shifting the position column one time period back.
-# The plot_allocation function creates a line plot of the close column from the input dataframe data, with points marked for trades using the trade column. It does this by adding two columns to the input dataframe data: flg_trade_buy and flg_trade_sell. The flg_trade_buy column is created by using np.where to set the value to the corresponding value in the close column if the value in the trade column is 1, and to np.nan otherwise. The flg_trade_sell column is created in a similar manner, using the value in the close column if the value in the trade column is 2. The function then uses the sns.lineplot and sns.scatterplot functions from the seaborn library to create a line plot of the close column and scatter plots for the flg_trade_buy and flg_trade_sell columns, using different marker shapes and colors to distinguish between buy and sell trades. Finally, it uses the ticker.MultipleLocator function to set the x-axis tick marks to be spaced 100 units apart.
-
-
-#########################################################################################
-#########################################################################################
-#########################################################################################
-
-
 class TradeAllocation:
+    """
+    This code defines three functions: signal, allocation, and plot_allocation. The signal function adds two columns to the input dataframe data: trigger and trigger_shift. The trigger column is created by using np.where to set the value to "BUY" if the value in the signal_EMA column is 1, "SELL" if the value in the signal_EMA column is 2, and "999" otherwise. The trigger_shift column is created by shifting the trigger column one time period back.
+    The allocation function adds three columns to the input dataframe data: trade, position, and position_shift. The trade column is created by using np.where to set the value to 1 if the current trigger value is "BUY" and the previous trigger value is not "BUY", to 2 if the current trigger value is "SELL" and the previous trigger value is not "SELL", and to 0 otherwise. The position column is created by using np.where to set the value to "long" if the trigger value is "BUY", "short" if the trigger value is "SELL", and np.nan otherwise. The position_shift column is created by shifting the position column one time period back.
+    The plot_allocation function creates a line plot of the close column from the input dataframe data, with points marked for trades using the trade column. It does this by adding two columns to the input dataframe data: flg_trade_buy and flg_trade_sell. The flg_trade_buy column is created by using np.where to set the value to the corresponding value in the close column if the value in the trade column is 1, and to np.nan otherwise. The flg_trade_sell column is created in a similar manner, using the value in the close column if the value in the trade column is 2. The function then uses the sns.lineplot and sns.scatterplot functions from the seaborn library to create a line plot of the close column and scatter plots for the flg_trade_buy and flg_trade_sell columns, using different marker shapes and colors to distinguish between buy and sell trades. Finally, it uses the ticker.MultipleLocator function to set the x-axis tick marks to be spaced 100 units apart.
+    """
+
     def __init__(self, data, close):
         self.data = data
         self.close = close
